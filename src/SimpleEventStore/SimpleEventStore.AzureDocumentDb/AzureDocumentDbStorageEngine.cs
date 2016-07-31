4b72ef6956d10c29dfa0d098a82f7c56b56a632e
Mike Gore <mikegore1000@msn.com>
2016-07-31 16:51:14 +0100

    Created skeleton projects for Azure Document Db support

---
+++ src/SimpleEventStore/SimpleEventStore.AzureDocumentDb/AzureDocumentDbStorageEngine.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SimpleEventStore.AzureDocumentDb
+{
+    public class AzureDocumentDbStorageEngine : IStorageEngine
+    {
+        public Task AppendToStream(string streamId, IEnumerable<StorageEvent> events)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<StorageEvent>> ReadStreamForwards(string streamId, int startPosition, int numberOfEventsToRead)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
