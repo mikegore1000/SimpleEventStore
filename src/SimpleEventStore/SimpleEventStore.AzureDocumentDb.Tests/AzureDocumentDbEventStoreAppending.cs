bd27a1376d3107a3ea7132e3b906eb24960e514d
Mike Gore <mikegore1000@msn.com>
2016-08-01 06:03:01 +0100

    Initial commit of work in progress DocumentDb storage engine (unstable)

---
+++ src/SimpleEventStore/SimpleEventStore.AzureDocumentDb.Tests/AzureDocumentDbEventStoreAppending.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents.Client;
+using SimpleEventStore.Tests;
+
+namespace SimpleEventStore.AzureDocumentDb.Tests
+{
+    //public class AzureDocumentDbEventStoreAppending : EventStoreAppending
+    //{
+    //    protected async override Task<IStorageEngine> CreateStorageEngine()
+    //    {
+    //        var documentDbUri = "https://simple-event-store.documents.azure.com:443/";
+    //        var authKey = "ehsCsRnC5MorINm4gUj6n6kozIVxtCh7z1NTPBZ6sBtY2hT9u23XiRnvSYnfKw0UzQZdSQ49PdL0BNKhKWjozw==";
+    //        var databaseName = "DocumentDbEventStoreTests";
+    //        DocumentClient client = new DocumentClient(new Uri(documentDbUri), authKey);
+
+    //        var storageEngine = new AzureDocumentDbStorageEngine(client, databaseName);
+    //        await storageEngine.Initialise();
+
+    //        return storageEngine;
+    //    }
+    //}
+}
--- src/SimpleEventStore/SimpleEventStore.AzureDocumentDb/AzureDocumentDbStorageEngine.cs
+++ src/SimpleEventStore/SimpleEventStore.AzureDocumentDb/AzureDocumentDbStorageEngine.cs
@@ -1,18 +1,79 @@
 ﻿using System.Collections.Generic;
+using System.Net;
 using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
 
 namespace SimpleEventStore.AzureDocumentDb
 {
     public class AzureDocumentDbStorageEngine : IStorageEngine
     {
+        private readonly IDocumentClient client;
+        private readonly string databaseName;
+
+        public AzureDocumentDbStorageEngine(IDocumentClient client, string databaseName)
+        {
+            this.client = client;
+            this.databaseName = databaseName;
+        }
+
+        public async Task Initialise()
+        {
+            await CreateDatabaseIfItDoesNotExist();
+            await CreateCollectionIfItDoesNotExist();
+        }
+
         public Task AppendToStream(string streamId, IEnumerable<StorageEvent> events)
         {
+            // TODO: Need to invoke a stored procedure in order to insert all the events in an atomic manner
             throw new System.NotImplementedException();
         }
 
         public Task<IEnumerable<StorageEvent>> ReadStreamForwards(string streamId, int startPosition, int numberOfEventsToRead)
         {
             throw new System.NotImplementedException();
+        }
+
+        private async Task CreateDatabaseIfItDoesNotExist()
+        {
+            try
+            {
+                await client.CreateDatabaseAsync(new Database { Id = databaseName });
+            }
+            catch (DocumentClientException ex)
+            {
+                if (ex.StatusCode != HttpStatusCode.Conflict)
+                {
+                    throw;
+                }
+            }
+        }
+
+        private async Task CreateCollectionIfItDoesNotExist()
+        {
+            // TODO: Need to optimise the indexing policy
+
+            var collection = new DocumentCollection
+            {
+                Id = "Commits"
+            };
+
+            // TODO: Make this configurable by the consuming app - need to see if this can be updated, if so then we should attempt to update
+            var requestOptions = new RequestOptions
+            {
+            };
+
+            try
+            {
+                await client.CreateDocumentCollectionAsync(UriFactory.CreateDatabaseUri(databaseName), collection, requestOptions);
+            }
+            catch (DocumentClientException ex)
+            {
+                if (ex.StatusCode != HttpStatusCode.Conflict)
+                {
+                    throw;
+                }
+            }
         }
     }
 }
