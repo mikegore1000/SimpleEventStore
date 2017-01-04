84669411f6450bb0cbff79c820359fb8c81da998
Mike Gore <mikegore1000@msn.com>
2017-01-04 23:00:05 +0000

    Moved Subscription class from an inner private class to an assembly internal class

--- src/SimpleEventStore/SimpleEventStore.AzureDocumentDb/AzureDocumentDbStorageEngine.cs
+++ src/SimpleEventStore/SimpleEventStore.AzureDocumentDb/AzureDocumentDbStorageEngine.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
 using Microsoft.Azure.Documents.Linq;
-using Newtonsoft.Json;
 
 namespace SimpleEventStore.AzureDocumentDb
 {
@@ -149,82 +148,6 @@ namespace SimpleEventStore.AzureDocumentDb
             subscriptions.Add(subscription);
 
             subscription.Start();
-        }
-
-        private class Subscription
-        {
-            private readonly DocumentClient client;
-            private readonly Uri commitsLink;
-            private readonly Action<StorageEvent> onNextEvent;
-            private readonly Action<string> onCheckpoint;
-            private readonly SubscriptionOptions subscriptionOptions;
-            private readonly Dictionary<string, string> checkpoints;
-            private Task workerTask;
-
-            public Subscription(DocumentClient client, Uri commitsLink, Action<StorageEvent> onNextEvent, Action<string> onCheckpoint, string checkpoint, SubscriptionOptions subscriptionOptions)
-            {
-                this.client = client;
-                this.commitsLink = commitsLink;
-                this.onNextEvent = onNextEvent;
-                this.onCheckpoint = onCheckpoint;
-                this.checkpoints = checkpoint == null ? new Dictionary<string, string>() : JsonConvert.DeserializeObject<Dictionary<string, string>>(checkpoint);
-                this.subscriptionOptions = subscriptionOptions;
-            }
-
-            // TODO: Configure the retry policy, also allow the subscription to be canclled (use a CancellationToken)
-            public void Start()
-            {
-                workerTask = Task.Run(async () =>
-                {
-                    while (true)
-                    {
-                        await ReadEvents();
-                        await Task.Delay(subscriptionOptions.PollEvery);
-                    }
-                });
-            }
-
-            private async Task ReadEvents()
-            {
-                var partitionKeyRanges = new List<PartitionKeyRange>();
-                FeedResponse<PartitionKeyRange> pkRangesResponse;
-
-                do
-                {
-                    pkRangesResponse = await client.ReadPartitionKeyRangeFeedAsync(commitsLink);
-                    partitionKeyRanges.AddRange(pkRangesResponse);
-                }
-                while (pkRangesResponse.ResponseContinuation != null);
-
-                foreach (var pkRange in partitionKeyRanges)
-                {
-                    string continuation;
-                    checkpoints.TryGetValue(pkRange.Id, out continuation);
-
-                    IDocumentQuery<Document> query = client.CreateDocumentChangeFeedQuery(
-                        commitsLink,
-                        new ChangeFeedOptions
-                        {
-                            PartitionKeyRangeId = pkRange.Id,
-                            StartFromBeginning = true,
-                            RequestContinuation = continuation,
-                            MaxItemCount = subscriptionOptions.MaxItemCount
-                        });
-
-                    while (query.HasMoreResults)
-                    {
-                        var feedResponse = await query.ExecuteNextAsync<Document>();
-
-                        foreach (var @event in feedResponse)
-                        {
-                            this.onNextEvent(DocumentDbStorageEvent.FromDocument(@event).ToStorageEvent());
-                        }
-
-                        checkpoints[pkRange.Id] = feedResponse.ResponseContinuation;
-                        this.onCheckpoint(JsonConvert.SerializeObject(checkpoints));
-                    }
-                }
-            }
         }
     }
 }
